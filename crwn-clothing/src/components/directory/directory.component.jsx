c7f0aa6dce916a453b5150259aa19b5760adb381
skywindzz <skywindzz@yahoo.com>
2020-01-07 00:46:03 -0800

    menu styles added + hompage component break down

--- crwn-clothing/src/components/directory/directory.component.jsx
+++ crwn-clothing/src/components/directory/directory.component.jsx
@@ -13,27 +13,23 @@ class Directory extends React.Component {
           imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
           id: 1,
           linkUrl: 'shop/hats'
-        },
-        {
+        }, {
           title: 'jackets',
           imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
           id: 2,
           linkUrl: 'shop/jackets'
-        },
-        {
+        }, {
           title: 'sneakers',
           imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
           id: 3,
           linkUrl: 'shop/sneakers'
-        },
-        {
+        }, {
           title: 'womens',
           imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
           size: 'large',
           id: 4,
           linkUrl: 'shop/womens'
-        },
-        {
+        }, {
           title: 'mens',
           imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
           size: 'large',
@@ -47,9 +43,11 @@ class Directory extends React.Component {
   render() {
     return (
       <div className='directory-menu'>
-        {this.state.sections.map(({ title, id, imageUrl, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
-        ))}
+        {/*notice here that here we use desctructure to get the prop items out usually you probably just use section as the prop holder */}
+        {this
+          .state
+          .sections
+          .map(({title, id, imageUrl, size}) => (<MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>))}
       </div>
     );
   }
