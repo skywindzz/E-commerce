fba889e53424623caf58f81bceeb8f35214abaf2
skywindzz <skywindzz@yahoo.com>
2020-01-18 19:57:34 -0800

    add forum-input component

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './App.scss';
 import HomePage from './pages/homepage.component';
-import { Switch, Route } from 'react-router-dom';
+import {Switch, Route} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/Header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 const HatsPage = () => (
   <div>
@@ -23,13 +25,15 @@ const HatsPage = () => (
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/HATS' component={HatsPage} />
-        <Route path='/Shop' component={ShopPage} />
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/HATS' component={HatsPage}/>
+        <Route path='/Shop' component={ShopPage}/>
+        <Route path='/signin' component={SignInAndSignUpPage}/>
       </Switch>
     </div>
-  );
+  )
 }
 
 export default App;
